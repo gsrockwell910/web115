08d51d7e87356d0edd3e8c06eb46295de9424702
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-09-28 13:37:31 -0400

    Remove deep nesting from dropdown menu code

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -27,19 +27,16 @@ function toggleMenu() {
     dropdownButton.textContent = untoggledText;
   }
 
-  // Close menu is user clicks off 
+  // Close menu if user clicks off 
   window.onclick = function(event) {
-    // If the target of the click is not the dropdown button
-    if (!event.target.matches(".dropdownButton")) {
-      // If the dropdown UL current has the class of show
-      if (dropdownUL.classList.contains("show")) {
+    // If the target of the click is not the dropdown button and the dropdown ul classlist contains show
+    if (!event.target.matches(".dropdownButton") && dropdownUL.classList.contains("show")) {
         // Untoggle
         toggled = false;
         // Remove class
         dropdownUL.classList.remove("show");
         // Change text
         dropdownButton.textContent = untoggledText;
-      }
     }
   };
 
@@ -48,16 +45,13 @@ function toggleMenu() {
     // If the target of the click is not the dropdown button, 
     // extra condition checks if object being touched is a dropdown link. 
     // without this links will not open in mobile view.
-    if (!event.target.matches(".dropdownButton") && !event.target.matches(".dropdown-link")) {
-      // If the dropdown UL current has the class of show
-      if (dropdownUL.classList.contains("show")) {
-        // Untoggle
-        toggled = false;
-        // Remove class
-        dropdownUL.classList.remove("show");
-        // Change text
-        dropdownButton.textContent = untoggledText;
-      }
+    if (!event.target.matches(".dropdownButton") && !event.target.matches(".dropdown-link") && dropdownUL.classList.contains("show")) {
+      // Untoggle
+      toggled = false;
+      // Remove class
+      dropdownUL.classList.remove("show");
+      // Change text
+      dropdownButton.textContent = untoggledText;
     }
   };
 }
