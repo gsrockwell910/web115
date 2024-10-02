8c246078dd0d3a48a7d38c063392b514e9077d58
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-10-02 13:53:18 -0500

    Remove nesting from dropdown methods

--- scripts/dropdown.js
+++ scripts/dropdown.js
@@ -6,13 +6,13 @@ const dropdownButton = document.querySelector(".dropdownButton");
 const toggledText = "⤴ FizzBuzz";
 const untoggledText = "⤵ FizzBuzz";
 // Flag
-let toggled = false;
+let isToggled = false;
 // Toggle class
 function toggleMenu() {
   // If state was not toggled
-  if (toggled === false) {
+  if (isToggled === false) {
     // Toggle it
-    toggled = true; 
+    isToggled = true; 
     // add show class to make list visible
     dropdownUL.classList.add("show");
     // Change text
@@ -20,7 +20,7 @@ function toggleMenu() {
     // State is toggled
   } else {
     // Untoggle it
-    toggled = false;
+    isToggled = false;
     // Remove class
     dropdownUL.classList.remove("show");
     // Change text
@@ -29,32 +29,30 @@ function toggleMenu() {
 
   // Close menu if user clicks off 
   window.onclick = function(event) {
-    // If the target of the click is not the dropdown button and the dropdown ul classlist contains show
-    if (!event.target.matches(".dropdownButton") && 
-        dropdownUL.classList.contains("show")) {
-        // Untoggle
-        toggled = false;
-        // Remove class
-        dropdownUL.classList.remove("show");
-        // Change text
-        dropdownButton.textContent = untoggledText;
-    }
+    // If the target of the click is the dropdown button and the dropdown ul classlist contains show - return (do nothing)
+    // Doing this removes another layer of nesting
+    if (event.target.matches(".dropdownButton") && dropdownUL.classList.contains("show")) return;
+    // Untoggle
+    isToggled = false;
+    // Remove class
+    dropdownUL.classList.remove("show");
+    // Change text
+    dropdownButton.textContent = untoggledText;
   };
 
   // Close menu on mobile if user touches any part of the window that is not the dropdown menu or dd menu links
   window.ontouchend = function(event) {
-    // If the target of the click is not the dropdown button, 
+    // If the target of the click is the dropdown button, 
     // extra condition checks if object being touched is a dropdown link. 
     // without this links will not open in mobile view.
-    if (!event.target.matches(".dropdownButton") && 
-        !event.target.matches(".dropdown-link") && 
-        dropdownUL.classList.contains("show")) {
-      // Untoggle
-      toggled = false;
-      // Remove class
-      dropdownUL.classList.remove("show");
-      // Change text
-      dropdownButton.textContent = untoggledText;
-    }
+    if (event.target.matches(".dropdownButton") && 
+        event.target.matches(".dropdown-link") &&
+        dropdownUL.classList.contains("show")) return;
+    // Untoggle
+    isToggled = false;
+    // Remove class
+    dropdownUL.classList.remove("show");
+    // Change text
+    dropdownButton.textContent = untoggledText;
   };
 }
