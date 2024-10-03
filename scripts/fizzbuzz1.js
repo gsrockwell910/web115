6d1cfb529f9005ad286d8605f0c42a2c89c1de78
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-10-02 21:27:19 -0500

    Update script

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -22,10 +22,11 @@ document.addEventListener("DOMContentLoaded", function () {
     const secondDivisor = 5;
     // Loop 
     for (let i = 1; i <= loopControl; i++){
-    // Prepend zeros if number is less than 3 digits
-    if (i <= 9) outputElement.innerHTML += "00";
-    if (i > 9 && i <= 99) outputElement.innerHTML += "0";
-    // Check for both conditions first to avoid incorrect output
+     // Prepend 2 zeros if number is 1 digit
+     if (i <= 9) outputElement.innerHTML += "00";
+     // Prepend 1 zero if number is 2 digits
+     if (i > 9 && i <= 99) outputElement.innerHTML += "0";
+     // Check for both conditions first to avoid incorrect output
      if (i % firstDivisor === 0 && i % secondDivisor === 0) {
       // Change text conent
       outputElement.innerHTML += `${i}) ${fullWord} - This number is divisible by ${firstDivisor} and ${secondDivisor}.<br>`;
