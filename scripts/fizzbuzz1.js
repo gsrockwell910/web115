7230b4efcef853058d46c06e30948703887b2e6e
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-10-02 21:25:18 -0500

    Prepend zeros for numbers less than 3 digits

--- scripts/fizzbuzz1.js
+++ scripts/fizzbuzz1.js
@@ -22,6 +22,9 @@ document.addEventListener("DOMContentLoaded", function () {
     const secondDivisor = 5;
     // Loop 
     for (let i = 1; i <= loopControl; i++){
+    // Prepend zeros if number is less than 3 digits
+    if (i <= 9) outputElement.innerHTML += "00";
+    if (i > 9 && i <= 99) outputElement.innerHTML += "0";
     // Check for both conditions first to avoid incorrect output
      if (i % firstDivisor === 0 && i % secondDivisor === 0) {
       // Change text conent
