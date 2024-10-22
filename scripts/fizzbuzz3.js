c70f0c7ad5be5fb01771c07ccb5502bb0648d1f2
gsrockwell910 <112737238+gsrockwell910@users.noreply.github.com>
2024-10-22 16:29:36 -0500

    Update js to be more modular

--- scripts/fizzbuzz3.js
+++ scripts/fizzbuzz3.js
@@ -11,20 +11,25 @@ document.addEventListener("DOMContentLoaded", function () {
     event.preventDefault(); 
     // Set magic number (140) in const
     const loopControl = 140;
+    let iterator = 1;
     // Create element to store output
     const outputElement = document.createElement("p");
     // Create variables for words
-    const firstWord = "Rhino";
-    const secondWord = "Vision";
-    const thirdWord = "BANG!";
-    const fullWord = "RhinoVision";
+    // const firstWord = "Rhino";
+    // const secondWord = "Vision";
+    // const thirdWord = "BANG!";
+    // const fullWord = "RhinoVision";
     // Strings to prepend to outputElement if i is less than 3 digits
     const oneDigitPrepend = "00";
     const twoDigitPrepend = "0";
     // Create variables for numbers
-    const firstDivisor = 3;
-    const secondDivisor = 5;
-    const thirdDivisor = 7;
+    // const firstDivisor = 3;
+    // const secondDivisor = 5;
+    // const thirdDivisor = 7;
+    const divisorMap = new Map();
+    divisorMap.set(3, "Rhino");
+    divisorMap.set(5, "Vision");
+    divisorMap.set(7, " BANG!");
     // Declare function (checkDivision)
     function checkDivision(dividend, divisor) {
       // Flag
@@ -32,37 +37,24 @@ document.addEventListener("DOMContentLoaded", function () {
       // Return boolean
       return isDivisible;
     }
-    // Loop 
-    for (let i = 1; i <= loopControl; i++){
-     // Prepend 2 zeros if number is 1 digit, 1 zero if num is 2 digits
-     (i <= 9) ? outputElement.innerHTML += oneDigitPrepend : (i > 9 && i <= 99) ? outputElement.innerHTML += twoDigitPrepend : "";
-     // Check for both conditions first to avoid incorrect output, use new function
-     if (checkDivision(i, firstDivisor) && checkDivision(i, secondDivisor) && checkDivision(i, thirdDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${fullWord} ${thirdWord} - This number is divisible by ${firstDivisor}, ${secondDivisor}, and ${thirdDivisor}.<br>`;
-     } else if (checkDivision(i, firstDivisor) && checkDivision(i, secondDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${fullWord} - This number is divisible by ${firstDivisor} and ${secondDivisor}.<br>`;
-     } else if (checkDivision(i, firstDivisor) && checkDivision(i, thirdDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${firstWord} ${thirdWord} - This number is divisible by ${firstDivisor} and ${thirdDivisor}.<br>`;
-     } else if (checkDivision(i, secondDivisor) && checkDivision(i, thirdDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${secondWord} ${thirdWord} - This number is divisible by ${secondDivisor} and ${thirdDivisor}.<br>`;
-     } else if (checkDivision(i, firstDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${firstWord} - This number is divisible by ${firstDivisor} only.<br>`;
-     } else if (checkDivision(i, secondDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${secondWord} - This number is divisible by ${secondDivisor} only.<br>`;
-     } else if (checkDivision(i, thirdDivisor)) {
-      // Change text conent
-      outputElement.innerHTML += `${i}) ${thirdWord} - This number is divisible by ${thirdDivisor} only.<br>`;
-     } else {
-      // Change text conent
-      outputElement.innerHTML += `${i}) This number is not divisible by ${firstDivisor}, ${secondDivisor}, or ${thirdDivisor}.<br>`;
-     }
-    }
+    // Outer Loop - controls total iterations
+    do {
+      // vars to hold assembled word and numbers that are divisible by the keys
+      let newWord = " ";
+      let divisibleBy = [];
+      // Prepend 2 zeros if number is 1 digit, 1 zero if num is 2 digits
+      (iterator <= 9) ? outputElement.innerHTML += oneDigitPrepend : (iterator > 9 && iterator <= 99) ? outputElement.innerHTML += twoDigitPrepend : "";
+      // inner loop compares i to each key of the hash, checkin for division
+      for (let [key, value] of divisorMap) {
+        if (checkDivision(iterator, key)) {
+            newWord += value;
+            divisibleBy.push(key);
+        } 
+      }
+      // assemble new word
+      newWord !== " " ? outputElement.innerHTML += `${iterator}) ${newWord} - This number is divisible by ${divisibleBy}.<br>` : outputElement.innerHTML += `${iterator}) This number is not divisible by any number in the hash.<br>`;
+      iterator++;
+    } while (iterator <= loopControl);
     // Clear output if there is any
     outputSection.innerHTML = "";
     //append element
